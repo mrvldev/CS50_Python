ef03b4bd3843c7275f7d62b4636e1bc5e556ab7a
mrvldev <mikerawvonl@gmail.com>
2025-10-25 16:46:15 +0200

    done some exercises

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,11 @@
-students = ["Hermione", "Harry", "Ron"]
+students = [
+    {"name": "Harry Potter", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Hermione Granger", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Ron Weasley", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco Malfoy", "house": "Slytherin", "patronus": None},
+    {"name": "Luna Lovegood", "house": "Ravenclaw", "patronus": "Hare"},
+    {"name": "Cedric Diggory", "house": "Hufflepuff", "patronus": "Badger"},   
+]
 
 for student in students:
-    print(student)
+    print(student["name"], student["house"], student["patronus"], sep=", ")
